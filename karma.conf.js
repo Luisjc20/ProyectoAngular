bc2cb8503e423660f1d00f71965e13345ebdecc4
Luisjc20 <luisimt20@hotmail.com>
2022-10-04 21:05:47 -0500

    v2

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/knowledges-app-20224169'),
+      dir: require('path').join(__dirname, './coverage/knowledges-app-20191e642'),
       subdir: '.',
       reporters: [
         { type: 'html' },
